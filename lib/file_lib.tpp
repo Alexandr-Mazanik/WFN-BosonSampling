1679f37fa8693c78ed6afcd97b856ec79ad5595b
Alexandr_Mazan <alexandrmazanik@gmail.com>
2024-08-30 14:23:09 +0300

    export data to file was changed
    now averages are not compute befor exporting

--- lib/file_lib.tpp
+++ lib/file_lib.tpp
@@ -17,3 +17,21 @@ void export_vec_to_file(std::vector<T> vec, std::string file_name) {
 	else
 		std::cout << "\n--> Couldn't open file\n";
 }
+
+template <typename T>
+void export_data(std::vector<std::vector<T>>& data, std::string file_name) {
+	std::ofstream f_data;
+
+	f_data.open("../../../analysis/" + file_name);
+
+	if (f_data.is_open()) {
+		for (std::vector<T> vec : data) {
+			for (T elem : vec)
+				f_data << elem << " "; 
+			f_data << std::endl;
+		}
+		std::cout << "\n--> Exported: " << file_name;
+	}
+	else
+		std::cout << "\n--> Couldn't open file\n";
+}
--- tests/CloudPositionN.cpp
+++ tests/CloudPositionN.cpp
@@ -5,14 +5,14 @@
 
 int main() {
 	int N_start = 1;
-	int N_end = 8001;
-	int N_step = 1000;
+	int N_end = 2001;
+	int N_step = 200;
 
 	int avg_amount = 5;
 
-	int ph_num = 5;
-	int dim_number = ph_num * ph_num;
-	int radius = 2;
+	int ph_num = 6;
+	int dim_number = 36;
+	int radius = 6;
 
 	StateSpace spaceBS(ph_num, dim_number);
 	StateSpace spaceUF(spaceBS);
@@ -21,7 +21,7 @@ int main() {
 	StateSpace spaceMF(ph_num, dim_number, false);
 
 	Scheme scheme(spaceBS);
-	scheme.ImportSchemeUnitary("scheme_unitary_5.txt");
+	scheme.ImportSchemeUnitary("scheme_unitary_6.txt");
 
 	std::vector<int> init_conf(ph_num, 1);
 	init_conf.resize(dim_number);
@@ -31,48 +31,51 @@ int main() {
 
 	std::vector<Sampler*> samplers = { 
 		new BFbosonSampler(&spaceBS, scheme, init_conf), 
-		new CoherentSampler(&spaceCH, scheme, init_conf_coherent), 
-		new UniformSampler(&spaceUF),
+		//new CoherentSampler(&spaceCH, scheme, init_conf_coherent), 
+		//new UniformSampler(&spaceUF),
 		new DistinguishableSampler(&spaceDS, scheme, init_conf),
 		new MeanFieldSampler(&spaceMF, scheme, init_conf)
 	};
 
-	std::vector<float> all_cloud_mu, all_cloud_sigma;
-	std::vector<float> avg_mu, avg_sigma;
+	//std::vector<float> all_cloud_mu, all_cloud_sigma;
+	std::vector<std::vector<float>> mu_data, sigma_data;
 
 	for (Sampler* sampler : samplers) {
 		std::cout << "\nSAMPLER: " << sampler->get_name() << "\n";
 		for (int N = N_start; N <= N_end; N += N_step) {
 			std::cout << "N: " << N << " / " << N_end << "\r";
 
+			mu_data.resize(mu_data.size() + 1);
+			sigma_data.resize(sigma_data.size() + 1);
+
 			for (int i = 0; i < avg_amount; ++i) {
 				sampler->sample(N);
 				Network network(sampler->space_ptr(), radius, false);
 				std::vector<float> two_moments = network.degDistTwoMoments(network.degreeDistribution());
 
-				all_cloud_mu.push_back(two_moments[0]);
-				all_cloud_sigma.push_back(two_moments[1]);
+				mu_data.back().push_back(two_moments[0]);
+				sigma_data.back().push_back(two_moments[1]);
 
 				sampler->space_ptr()->reset();
 			}
 
-			avg_mu.push_back(get_vec_avg(all_cloud_mu));
-			avg_sigma.push_back(get_vec_avg(all_cloud_sigma));
+			//avg_mu.push_back(get_vec_avg(all_cloud_mu));
+			//avg_sigma.push_back(get_vec_avg(all_cloud_sigma));
 
-			all_cloud_mu.clear();
-			all_cloud_sigma.clear();	
+			//all_cloud_mu.clear();
+			//all_cloud_sigma.clear();	
 		}
 		
-		std::string file_name_mu = "cloud_position_N/data/cloud_mu_5_N_" + std::to_string(N_start) + "_" + 
+		std::string file_name_mu = "cloud_position_N/data/cloud_mu_6_r6_N_" + std::to_string(N_start) + "_" + 
 			std::to_string(N_end) + "_" + std::to_string(N_step) + "_" + sampler->get_name() + ".txt";
-		std::string file_name_sigma = "cloud_position_N/data/cloud_sigma_5_N_" + std::to_string(N_start) + "_" + 
+		std::string file_name_sigma = "cloud_position_N/data/cloud_sigma_6_r6_N_" + std::to_string(N_start) + "_" + 
 			std::to_string(N_end) + "_" + std::to_string(N_step) + "_" + sampler->get_name() + ".txt";
 
-		export_vec_to_file<float>(avg_mu, file_name_mu);
-		export_vec_to_file<float>(avg_sigma, file_name_sigma);
+		export_data<float>(mu_data, file_name_mu);
+		export_data<float>(sigma_data, file_name_sigma);
 
-		avg_mu.clear();
-		avg_sigma.clear();
+		mu_data.clear();
+		sigma_data.clear();
 
 		std::cout << "\n--> OK\n";
 	}
