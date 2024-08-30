11f6258567fb014d03a295fe916471c63699a073
Alexandr_Mazan <alexandrmazanik@gmail.com>
2024-08-30 15:07:43 +0300

    MISampler constructor implemented

--- lib/samplers/MISampler.h
+++ lib/samplers/MISampler.h
@@ -1,9 +1,24 @@
 #pragma once
 
 #include "Sampler.h"
+#include "space/StateSpace.h"
+#include "interferometer/Scheme.h"
 
 class MISampler : public Sampler {
 public:
+	MISampler(StateSpace* space, Scheme& scheme, std::vector<int>& init_state);
+
+	void sample(int batch_size) override;
+
+	StateSpace* space_ptr() override;
+
+	std::string get_name() override;
+
 private:
+	int ph_num_, modes_num_;
+	Scheme scheme_;
+	StateSpace* space_ptr_;
+
+	std::vector<int> init_state_;
 };
 
